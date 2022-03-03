7998ad0e6405ef9ea939af0ce82e3f2157b11b44
DahliaL <dahlia.lapom@gmail.com>
2022-03-03 18:32:30 -0500

    first draft of algo
    infinite loop is occurring, not working by any means

--- minotaurBirthday.java
+++ minotaurBirthday.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class minotaurBirthday {
     private static long N;
+    private static boolean[] visited;
+    private static boolean plateWithCupcake = true;
+    private static int leaderNum;
+    private static int count = 0;
+    private static int curr;
 
     /*
     * The Minotaur invited N guests to his birthday party.
@@ -25,21 +32,76 @@ public class minotaurBirthday {
     Create a program to simulate the winning strategy (protocol) where each guest is represented by one running thread.
     * In your program you can choose a concrete number for N or ask the user to specify N at the start.
     * */
+    public static boolean verifyEnd() {
+        boolean valid = true;
+        for (boolean b : visited) {
+            if (!b) {
+                System.out.print("The Minotaur is not satisfied!");
+                valid = false;
+                break;
+            }
+        }
+
+        return valid;
+    }
+
+    public static void eatOrReplace(int guestID) {
+        if(!visited[guestID] && plateWithCupcake) { // if this guest has never been in and the plate has a cupcake..
+            visited[guestID] = true;
+            plateWithCupcake = false;
+        }
+        // in every other case, the guest should not do anything else... unless...
+        if(guestID == leaderNum) { // if this guest is the designated leader, they need to keep track!
+            if(!plateWithCupcake) { // someone ate the cupcake lol.. new guests came in, we need to increment
+                count++;
+                plateWithCupcake = true; // replacing the cupcake
+            }
+        }
+    }
 
     public static class guestAtParty implements Runnable {
         @Override
         public synchronized void run() {
-
+            eatOrReplace(curr -  1);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.print("Enter the number of guests attending the Minotaur's birthday party (must be greater than 0): ");
         Scanner sc = new Scanner(System.in);
         N = sc.nextLong();
         sc.close();
 
+        visited = new boolean[(int)N];
+        Arrays.fill(visited, false);
 
+        // after some deliberating, the guests agree that they should delegate a leader
+        leaderNum = ThreadLocalRandom.current().nextInt(0, (int)N-1); // choosing a random guest to be the leader
+
+        guestAtParty guestAtParty = new guestAtParty();
+        Thread[] guests = new Thread[(int)N];
+
+        // the guest are ready to play!
+        int entries = 0; // this is for my own reason to verify that around n^2 entries happen
+
+        while(count < N) {
+            curr = ThreadLocalRandom.current().nextInt(0, (int)N-1); // simulating a random guest getting chosen to enter
+
+            try {
+                guests[curr].join();}
+            catch(NullPointerException | InterruptedException ignored) {
+            }
+
+            guests[curr] = new Thread(guestAtParty, "Thread"+curr);
+            guests[curr].start();
+
+            entries++;
+        }
+
+        System.out.print("The guests have decided to tell the Minotaur that all guests have entered the labyrinth. Is he satisfied?");
+
+        if(verifyEnd())
+            System.out.print("The Minotaur is satisfied! This took " + entries + "entries from the guests.");
 
     }
 }
